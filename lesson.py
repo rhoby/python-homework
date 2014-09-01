b396d02197e480216af089196108797ef561cf98
hoby <hoby@aims.edu.gh>
2014-09-01 13:30:50 +0000

    drawing shapes

---
+++ lesson.py
@@ -0,0 +1,12 @@
+import turtle
+
+side = 50
+
+i = 0;
+while (i < 4):
+    turtle.forward(side)
+    turtle.left(90)
+    i = i+1
+
+turtle.done()
+__author__ = 'hoby'
---
+++ mathplotlib/mathplotlib.py
@@ -0,0 +1,22 @@
+__author__ = 'hoby'
+from pylab import *
+
+from square_geom import *
+
+v_square_area = np.vectorize(square_area)
+v_square_perimeter = np.vectorize(square_perimeter)
+
+S = np.linspace(0,10) # our side lengths
+
+A = v_square_area(S)  # the areas
+P = v_square_perimeter(S)  # the perimeters
+
+plot(S, A, '-r', label="Area")
+plot(S, P, ':b', label="Perimeter")
+
+xlabel('side length')
+ylabel('geo values')
+title('Square Geo Properties')
+legend(loc='upper right')
+
+show()
